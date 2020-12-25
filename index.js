b64f2175abc0373cea4bb34b54f436e1bf767f68
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2020-12-25 16:58:38 +0100

    Refactoring padStart

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const r = l => {
+function* t(ml, r, ps) {
+    yield ps('+', ml + 1);
+    for (let i = 1; i <= ml; i += 1) {
+        yield ps(r(i),ml) + r(i + 1);
+    }
+    yield ps('|||', ml + 2);
+};
+console.log(Array.from(t(8, l => {
     let rt = '';
     for (let i = 0; i < l; i += 1) {
         rt += Math.random() < 0.3 ? '*' : 'x';
     }
     return rt;
-};
-function* t(ml) {
-    yield '+'.padStart(ml + 1);
-    for (let i = 1; i <= ml; i += 1) {
-        yield r(i).padStart(ml) + r(i + 1);
-    }
-    yield '|||'.padStart(ml + 2);
-};
-console.log(Array.from(t(8)).join('\n'));
+}, (r, l) => r.padStart(l))).join('\n'));
